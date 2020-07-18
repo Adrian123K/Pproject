a145334c8f83f43cded21a53dfa9c1807c20b20e
AdrianKim <seojun.a.kim@gmail.com>
2020-07-18 18:53:03 +0900

    add pt files

--- ts.R
+++ ts.R
@@ -1,5 +1,6 @@
 setwd('d:/Pproject')
 setwd('d:/Desktop/Itwill ws/Pproject')
+setwd('C:/Users/seoju/Documents/ws/Pproject')
 
 install.packages('TTR')
 install.packages('forecast')
@@ -12,27 +13,12 @@ library(readxl)
 library(tseries)
 library(ggplot2)
 
-sample <- read.csv('5g 가입자.csv',header=F,stringsAsFactors=F)
-st <- t(sample[2:5,])
-st <- lapply(st,as.numeric)
-st <- as.matrix(st,nrow=14,ncol=4)
-st
-str(st)
-
-
-
-for (i in nrow(date)){
-  date[i] <- as.Date()
-}
-  
 sample <- read.csv('5g 가입자.csv',header=F, stringsAsFactors=F)
 
-Date <- t(sample[1,])
 SKT <- t(sample[2,])
 KT <- t(sample[3,])
 LG <- t(sample[4,])
 MVNO <- t(sample[5,])
-
 
 Date <- seq(as.Date('2019-04-01','%Y-%m-%d'),as.Date('2020-05-01','%Y-%m-%d'),by='month')
 SKT <- as.numeric(SKT)
@@ -44,14 +30,17 @@ st
 
 #date <- seq(as.Date('2019-04','%Y-%m'),as.Date('2020-05','%Y-%m'),'month')
 
-par(mfrow=c(2,1))
+
 
 ts_skt <- ts(st$SKT,start=c(2019,4),frequency = 12)
 end(ts_skt)
+
 dev.new()
+par(mfrow=c(2,1))
 plot.ts(ts_skt)
 abline(reg=lm(ts_skt~time(ts_skt)))
 plot(decompose(ts_skt))
+
 adf.test(ts_skt,alternative='stationary',k=0)
 adf.test(diff(log(ts_skt)),alternative='stationary',k=0)
 dev.new()
@@ -67,11 +56,11 @@ plot.ts(skt_d1)
 plot.ts(skt_d2)
 
 dev.new()
-par(mfrow=c(2,2))
+par(mfrow=c(2,1))
 acf(log(ts_skt))
 pacf(log(ts_skt))
-acf(diff(log(ts_skt)))
-pacf(diff(log(ts_skt)))
+ggAcf(diff(log(ts_skt)),lag=7)
+ggPacf(diff(log(ts_skt)),lag=7)
 
 ggAcf(ts_skt,lag=14)
 ggPacf(ts_skt,lag=14)
@@ -79,6 +68,10 @@ ggPacf(ts_skt,lag=14)
 dev.new()
 par(mfrow=c(2,2))
 acf(ts_skt)
+
+
+
+
 
 
 
---
+++ ts_4,5g.R
@@ -0,0 +1,146 @@
+setwd('C:/Users/seoju/Documents/ws/Pproject')
+getwd()
+library(TTR)
+library(forecast)
+library(data.table)
+library(readxl)
+library(tseries)
+library(ggplot2)
+
+## Data 붙이기
+skt4 <- read.csv('4g_sk.csv',header=F,stringsAsFactors = F)
+Date <- seq(as.Date('2013-01-01','%Y-%m-%d'),as.Date('2020-05-01','%Y-%m-%d'),'months')
+skt4 <- skt4[2,]
+skt4 <- as.numeric(skt4)
+
+df_sk <- data.frame(Date,skt4)
+str(df_sk)
+
+## skt data 분기별 freq
+ts_skt4 <- ts(df_sk$skt,start=c(2013,1),frequency = 12)
+end(ts_skt4)
+
+dev.new()
+plot.ts(ts_skt4)
+plot(decompose(ts_skt4))
+
+## 각 plot 확인
+dev.new()
+ggseasonplot(ts_skt4)
+ggsubseriesplot(diff(ts_skt4, differences = 1))
+gglagplot(ts_skt4)
+
+## acf, pacf 확인
+dev.new()
+par(mfrow=c(2,1))
+#acf(ts_skt4)
+#pacf(ts_skt4)
+ggAcf(diff(ts_skt4,differences = 1),lag=40) + ggtitle('차분 1회 시행 시 ACF')
+ggAcf(diff(ts_skt4,differences = 2),lag=40) + ggtitle('차분 2회 시행 시 ACF')
+ggPacf(diff(ts_skt4, differences = 1),lag=40)
+
+##### skt 4g
+auto.arima(ts_skt4,seasonal = F, stepwise=F, approximation = F)
+ggtsdisplay(diff(ts_skt4,differences = 2))
+
+skt4_fit <- arima(diff(ts_skt4),c(4,2,0))
+dev.new()
+checkresiduals(skt4_fit)
+
+dev.new()
+ts_skt4 %>%
+  Arima(order=c(4,2,0)) %>%
+  forecast() %>%
+  autoplot()
+# par(new=T)  
+
+##### skt 5g
+skt5 <- read.csv('5g_sk.csv',header=F,stringsAsFactors = F)
+Date <- seq(as.Date('2013-01-01','%Y-%m-%d'),as.Date('2020-05-01','%Y-%m-%d'),'months')
+skt5 <- as.numeric(skt5)
+
+df_skt5 <- data.frame(Date,skt5)
+
+ts_skt5 <- ts(df_skt5$skt5,start=c(2013,1),frequency = 12)
+end(ts_skt5)
+
+dev.new()
+plot.ts(ts_skt5)
+
+ggAcf(ts_skt5,lag=30)
+ggPacf(ts_skt5,lag=30)
+
+auto.arima(ts_skt5,seasonal = F, stepwise=F, approximation = F)
+ggtsdisplay(diff(ts_skt5,differences = 2))
+
+skt5_test <- arima(diff(ts_skt5,differences = 2),c(5,2,0))
+checkresiduals(skt5_test)
+
+dev.new()
+ts_skt5 %>%
+  Arima(order=c(5,2,0)) %>%
+  forecast() %>%
+  autoplot() + ggtitle('SKT 5g 예측 모델')
+
+
+
+
+dev.new()
+ts_skt4 %>%
+  Arima(order=c(4,2,0)) %>%
+  forecast() %>%
+  autoplot()+ ggtitle('SKT 4g 예측 모델')
+
+
+##### kt 4g
+
+kt4 <- read.csv('4g_kt.csv',header=F,stringsAsFactors = F)
+kt4 <- as.numeric(kt4)
+Date <- seq(as.Date('2013-01-01','%Y-%m-%d'),as.Date('2020-05-01','%Y-%m-%d'),'months')
+df_kt <- data.frame(Date, kt4)
+ts_kt4 <- ts(df_kt$kt4,start=c(2013,1),frequency = 12)
+
+# auto arima로 확인
+auto.arima(ts_kt4,seasonal = F, stepwise=F, approximation = F)
+# ARIMA(0,2,1)
+
+kt4_fit <- arima(diff(ts_kt4,differences = 2),c(2,2,0),)
+dev.new()
+checkresiduals(kt4_fit)
+autoplot(ts_kt4)
+
+dev.new()
+ggtsdisplay(diff(ts_kt4,differences = 2))
+
+dev.new()
+ts_kt4 %>%
+  Arima(order=c(2,2,0)) %>%
+  forecast() %>%
+  autoplot() + ggtitle('KT 4g 예측 모델')
+
+##### kt 5g
+kt5 <- read.csv('5g_kt.csv',header=F,stringsAsFactors = F)
+Date <- seq(as.Date('2013-01-01','%Y-%m-%d'),as.Date('2020-05-01','%Y-%m-%d'),'months')
+kt5 <- as.numeric(kt5)
+
+df_kt5 <- data.frame(Date,kt5)
+
+ts_kt5 <- ts(df_kt5$kt5,start=c(2013,1),frequency = 12)
+end(ts_kt5)
+
+dev.new()
+plot.ts(ts_kt5)
+
+ggAcf(ts_kt5,lag=30)
+ggPacf(ts_kt5,lag=30)
+
+auto.arima(ts_kt5,seasonal = F, stepwise=F, approximation = F)
+ggtsdisplay(diff(ts_kt5,differences = 2))
+
+kt5_test <- arima(diff(ts_kt5,differences = 2),c(5,2,0))
+checkresiduals(kt5_test)
+
+ts_kt5 %>%
+  Arima(order=c(5,2,0)) %>%
+  forecast() %>%
+  autoplot()+ ggtitle('KT 5g 예측 모델')
