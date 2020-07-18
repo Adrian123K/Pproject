ee66bcc9ff9e8142d1216e1622a046fb67a44418
AdrianKim <seojun.a.kim@gmail.com>
2020-07-18 19:46:06 +0900

    md final

--- ts_4,5g.R
+++ ts_4,5g.R
@@ -33,17 +33,24 @@ gglagplot(ts_skt4)
 ## acf, pacf 확인
 dev.new()
 par(mfrow=c(2,1))
-#acf(ts_skt4)
-#pacf(ts_skt4)
+acf(diff(ts_skt4,differences = 1),lag=40)
+pacf(diff(ts_skt4,differences = 1),lag=40)
+acf(diff(ts_skt4,differences = 2))
+pacf(diff(ts_skt4,differences = 2))
 ggAcf(diff(ts_skt4,differences = 1),lag=40) + ggtitle('차분 1회 시행 시 ACF')
 ggAcf(diff(ts_skt4,differences = 2),lag=40) + ggtitle('차분 2회 시행 시 ACF')
-ggPacf(diff(ts_skt4, differences = 1),lag=40)
+ggPacf(diff(ts_skt4, differences = 1),lag=40) + ggtitle('차분 1회 시행 시 PACF')
+ggPacf(diff(ts_skt4, differences = 1),lag=40) + ggtitle('차분 2회 시행 시 PACF')
 
 ##### skt 4g
 auto.arima(ts_skt4,seasonal = F, stepwise=F, approximation = F)
-ggtsdisplay(diff(ts_skt4,differences = 2))
+dev.new()
+ggtsdisplay(ts_skt4)
+dev.new()
+ggtsdisplay(diff(log(ts_skt4),differences = 2))
 
-skt4_fit <- arima(diff(ts_skt4),c(4,2,0))
+skt4_fit <- arima(diff(log(ts_skt4),differences = 2),c(4,2,0))
+summary(skt4_fit)
 dev.new()
 checkresiduals(skt4_fit)
 
@@ -51,7 +58,7 @@ dev.new()
 ts_skt4 %>%
   Arima(order=c(4,2,0)) %>%
   forecast() %>%
-  autoplot()
+  autoplot() + ggtitle('SKT 4G 예측 모델')
 # par(new=T)  
 
 ##### skt 5g
@@ -71,9 +78,12 @@ ggAcf(ts_skt5,lag=30)
 ggPacf(ts_skt5,lag=30)
 
 auto.arima(ts_skt5,seasonal = F, stepwise=F, approximation = F)
+dev.new()
 ggtsdisplay(diff(ts_skt5,differences = 2))
 
+
 skt5_test <- arima(diff(ts_skt5,differences = 2),c(5,2,0))
+dev.new()
 checkresiduals(skt5_test)
 
 dev.new()
@@ -100,11 +110,20 @@ Date <- seq(as.Date('2013-01-01','%Y-%m-%d'),as.Date('2020-05-01','%Y-%m-%d'),'m
 df_kt <- data.frame(Date, kt4)
 ts_kt4 <- ts(df_kt$kt4,start=c(2013,1),frequency = 12)
 
+# 원본데이터 확인
+dev.new()
+ggtsdisplay(ts_kt4)
+dev.new()
+ggtsdisplay(diff(log(ts_kt4),differences = 2),lag=10)
+
 # auto arima로 확인
 auto.arima(ts_kt4,seasonal = F, stepwise=F, approximation = F)
 # ARIMA(0,2,1)
 
-kt4_fit <- arima(diff(ts_kt4,differences = 2),c(2,2,0),)
+kt4_fit <- arima(diff(log(ts_kt4),differences = 2),c(2,2,0),)
+
+adf.test(diff(ts_kt4,differences = 2),alternative='stationary',k=0)
+summary(kt4_fit)
 dev.new()
 checkresiduals(kt4_fit)
 autoplot(ts_kt4)
@@ -116,7 +135,7 @@ dev.new()
 ts_kt4 %>%
   Arima(order=c(2,2,0)) %>%
   forecast() %>%
-  autoplot() + ggtitle('KT 4g 예측 모델')
+  autoplot() + ggtitle('KT 4G 예측 모델')
 
 ##### kt 5g
 kt5 <- read.csv('5g_kt.csv',header=F,stringsAsFactors = F)
@@ -129,7 +148,7 @@ ts_kt5 <- ts(df_kt5$kt5,start=c(2013,1),frequency = 12)
 end(ts_kt5)
 
 dev.new()
-plot.ts(ts_kt5)
+ggtsdisplay(ts_kt5)
 
 ggAcf(ts_kt5,lag=30)
 ggPacf(ts_kt5,lag=30)
